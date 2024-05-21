6ceed1c669fca2396f9d0e8b19e41c6c66aeb379
Adeu5 <saandi536@gmail.com>
2024-05-21 07:53:00 +0700

    test

---
+++ belajar_in/lib/widgets/description_section.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class DescriptionSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(top: 20),
+      child: Column(
+        children: [
+          Text(
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.black.withOpacity(0.7),
+            ),
+            textAlign: TextAlign.justify,
+          ),
+          SizedBox(height: 20),
+          Row(
+            children: [
+              Text(
+                "Course Length: ",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(width: 5),
+              Icon(
+                Icons.timer,
+                color: Color(0xFF674AEF),
+              ),
+              SizedBox(width: 5),
+              Text(
+                "24 Hours",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 10),
+          Row(
+            children: [
+              Text(
+                "Rating: ",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(width: 5),
+              Icon(
+                Icons.star,
+                color: Colors.amber,
+              ),
+              SizedBox(width: 5),
+              Text(
+                "4.7",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
